a757fc1f926a1af9e312dc594b7984ca2529539b
alphab01 <kp1682674@gmail.com>
2026-06-09 19:30:02 +0300

    init weights + hid layer adding

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+np.random.seed(1)
+
 def relu(x):
   return (x > 0) * x
 def relu2deriv(ou):
@@ -21,3 +23,6 @@ data = np.array([[0,0,0,0,1,
                   0,1,0,1,0,
                   0,0,1,0,0]])
 g = np.array([[0,1], [0,1], [1,0])
+h = 25
+we01 = 2 * np.random.random((25, h)) - 1
+we12 = 2 * np.random.random((h, 2)) - 1
